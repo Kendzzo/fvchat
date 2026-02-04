e718ea607e7da2dc97a4b745c79d56dcd80e93ba
Kendzzo <r.oyacornejo@gmail.com>
2026-02-05 00:42:08 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,16 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/fvchat/", // 👈 MUY IMPORTANTE para GitHub Pages
+  base: "/fvchat/", // 👈 MUY IMPORTANTE para GitHub Pages (repo name)
+
+  server: {
+    host: "::",
+    port: 8080,
+    hmr: { overlay: false },
+  },
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
